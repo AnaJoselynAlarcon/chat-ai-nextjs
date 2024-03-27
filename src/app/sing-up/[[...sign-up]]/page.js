cdd32063ed6be890a8931ca0f4b1859f4614ae06
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2024-03-27 11:20:58 -0600

    started

---
+++ src/app/sing-up/[[...sign-up]]/page.js
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SingUpPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SignUp appearance={{ variables: { colorPrimary: "#0F172A" } }} />
+    </div>
+  );
+}
